b6894f2fc8e152ce3d4d3216a2773d2f219c738e
Darrious <60057825+master-lzh@users.noreply.github.com>
2025-10-22 17:45:59 +0800

    feat(theme): 替换主题为新的表达式色彩方案以优化视觉效果
    - 将`lightColorScheme`替换为`expressiveLightColorScheme`以适配新的主题风格
    - 调整默认主题逻辑，增强视觉表达力并提升用户体验

--- app/src/main/kotlin/com/mrl/pixiv/theme/Theme.kt
+++ app/src/main/kotlin/com/mrl/pixiv/theme/Theme.kt
@@ -8,7 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.expressiveLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
@@ -26,7 +26,7 @@ fun PiPixivTheme(
         }
 
         darkTheme -> darkColorScheme()
-        else -> lightColorScheme()
+        else -> expressiveLightColorScheme()
     }
     MaterialExpressiveTheme(
         colorScheme = colorScheme,
