2e205732f6921bf054fe9513984d94288b010950
Darrious <60057825+master-lzh@users.noreply.github.com>
2025-10-29 17:30:43 +0800

    feat(ui): 重构导航组件以优化代码结构和适配能力
    - 使用`NavigationSuite`和`NavigationSuiteScaffold`替换原导航逻辑，简化组件结构，提高代码可读性
    - 删除多余的动画相关逻辑，移除不必要的布局约束以简化代码
    - 统一底部导航栏及页面布局逻辑，增强多屏幕适配的一致性
    - 更新`compose`相关依赖版本，确保新功能的兼容性和构建稳定性

--- lib_feature/src/main/kotlin/com/mrl/pixiv/MainScreen.kt
+++ lib_feature/src/main/kotlin/com/mrl/pixiv/MainScreen.kt
@@ -5,17 +5,14 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.DrawerDefaults
-import androidx.compose.material3.NavigationBarDefaults
-import androidx.compose.material3.NavigationRailDefaults
-import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldLayout
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
+import androidx.compose.foundation.layout.requiredHeightIn
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.mrl.pixiv.common.compose.ui.bar.HomeBottomBar
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.mrl.pixiv.common.router.MainScreenPage
 import com.mrl.pixiv.common.router.NavigationManager
 import com.mrl.pixiv.home.HomeScreen
@@ -30,53 +27,49 @@ fun MainScreen(
 ) {
     val navigationManager = koinInject<NavigationManager>()
     val page = navigationManager.currentMainPage
-    val windowAdaptiveInfo = currentWindowAdaptiveInfo()
-    val layoutType = NavigationSuiteScaffoldDefaults.calculateFromAdaptiveInfo(windowAdaptiveInfo)
-    NavigationSuiteScaffoldLayout(
-        navigationSuite = {
-            HomeBottomBar(
-                layoutType = layoutType,
-                currentPage = navigationManager.currentMainPage,
-                onSwitch = { destination ->
-                    navigationManager.switchMainPage(destination)
-                }
-            )
+    val screens = remember {
+        listOf(
+            MainScreenPage.HOME,
+            MainScreenPage.LATEST,
+            MainScreenPage.SEARCH,
+            MainScreenPage.PROFILE,
+        )
+    }
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            screens.forEach { screen ->
+                item(
+                    selected = page == screen,
+                    onClick = {
+                        if (page != screen) {
+                            navigationManager.switchMainPage(screen)
+                        }
+                    },
+                    icon = screen.icon,
+                    modifier = Modifier.requiredHeightIn(max = 56.dp),
+                    label = {
+                        Text(text = stringResource(screen.title))
+                    }
+                )
+            }
         },
-        layoutType = layoutType
     ) {
-        Box(
-            Modifier.consumeWindowInsets(
-                when (layoutType) {
-                    NavigationSuiteType.NavigationBar ->
-                        NavigationBarDefaults.windowInsets.only(WindowInsetsSides.Bottom)
+        AnimatedContent(
+            targetState = page,
+            modifier = modifier,
+            transitionSpec = {
+                fadeIn(animationSpec = tween(220, delayMillis = 90))
+                    .togetherWith(fadeOut(animationSpec = tween(90)))
 
-                    NavigationSuiteType.NavigationRail ->
-                        NavigationRailDefaults.windowInsets.only(WindowInsetsSides.Start)
-
-                    NavigationSuiteType.NavigationDrawer ->
-                        DrawerDefaults.windowInsets.only(WindowInsetsSides.Start)
-
-                    else -> WindowInsets(0, 0, 0, 0)
-                }
-            )
+            }
         ) {
-            AnimatedContent(
-                targetState = page,
-                modifier = modifier,
-                transitionSpec = {
-                    fadeIn(animationSpec = tween(220, delayMillis = 90))
-                        .togetherWith(fadeOut(animationSpec = tween(90)))
-
-                }
-            ) {
-                when (it) {
-                    MainScreenPage.HOME -> HomeScreen()
-                    MainScreenPage.LATEST -> LatestScreen()
-                    MainScreenPage.SEARCH -> SearchPreviewScreen()
-                    MainScreenPage.PROFILE -> ProfileScreen()
-                }
+            when (it) {
+                MainScreenPage.HOME -> HomeScreen()
+                MainScreenPage.LATEST -> LatestScreen()
+                MainScreenPage.SEARCH -> SearchPreviewScreen()
+                MainScreenPage.PROFILE -> ProfileScreen()
             }
         }
     }
-
 }
