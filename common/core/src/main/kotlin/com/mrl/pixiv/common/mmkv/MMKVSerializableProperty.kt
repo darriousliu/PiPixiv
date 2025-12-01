5166af44af9357b0e2de0c3b34061714ae1c220b
Darrious <60057825+master-lzh@users.noreply.github.com>
2025-12-01 17:29:52 +0800

    feat(mmkv): 优化数据读取与迁移逻辑
    - 简化了JSON和Protobuf数据的读取过程
    - 移除了冗余的异常处理，提升代码可读性
    - 确保旧数据能够正确迁移到JSON格式

--- common/core/src/main/kotlin/com/mrl/pixiv/common/mmkv/MMKVSerializableProperty.kt
+++ common/core/src/main/kotlin/com/mrl/pixiv/common/mmkv/MMKVSerializableProperty.kt
@@ -29,23 +29,15 @@ class MMKVSerializableProperty<V>(
         if (isJson) {
             val jsonString = thisRef.kv.getString(property.name, "")
             if (jsonString.isNotEmpty()) {
-                try {
-                    return mmkvJson.decodeFromString(serializer, jsonString)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
+                return mmkvJson.decodeFromString(serializer, jsonString)
             }
         } else {
             // 如果标记为 false，说明可能是旧的 Protobuf 数据，或者是新数据但还未写入过
             thisRef.kv.getByteArray(property.name)?.let {
-                try {
-                    val value = mmkvProtobuf.decodeFromByteArray(serializer, it)
-                    // 迁移：读取到旧数据后，以 JSON 格式写回，并更新标记
-                    setValue(thisRef, property, value)
-                    return value
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
+                val value = mmkvProtobuf.decodeFromByteArray(serializer, it)
+                // 迁移：读取到旧数据后，以 JSON 格式写回，并更新标记
+                setValue(thisRef, property, value)
+                return value
             }
         }
         return defaultValue
