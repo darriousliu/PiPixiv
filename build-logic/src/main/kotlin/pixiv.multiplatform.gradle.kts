bb2125c8b2e05c5e471edf0a074a561203ea5ce0
Darrious <60057825+master-lzh@users.noreply.github.com>
2025-12-24 17:03:26 +0800

    feat(build): 更新JvmTarget及依赖版本
    - 将JvmToolchain版本从25降至21，调整JvmTarget至JVM_21以提升兼容性。
    - 升级mmkv-kotlin至1.4.0版本，改进多平台数据存储能力。
    - 更新Android Gradle Plugin (AGP)版本至8.13.2，优化构建性能与稳定性。

--- build-logic/src/main/kotlin/pixiv.multiplatform.gradle.kts
+++ build-logic/src/main/kotlin/pixiv.multiplatform.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 kotlin {
-    jvmToolchain(25)
+    jvmToolchain(21)
     compilerOptions {
         freeCompilerArgs.addAll(optIns)
     }
@@ -33,7 +33,7 @@ kotlin {
 
     jvm {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_22)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
 
