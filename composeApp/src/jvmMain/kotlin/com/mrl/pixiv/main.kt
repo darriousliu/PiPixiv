dfdba97ae5e46993fb3258a0a44f4b1ad57c965d
Darrious <60057825+master-lzh@users.noreply.github.com>
2025-12-27 18:54:24 +0800

    feat(app): 支持默认语言设置初始化
    - 新增`setDefaultLocale`函数，用于设置默认语言环境。
    - 集成`SettingRepository`获取用户语言偏好并切换Locale。
    - 优化多语言初始化体验，加强语言设置一致性与兼容性。

--- composeApp/src/jvmMain/kotlin/com/mrl/pixiv/main.kt
+++ composeApp/src/jvmMain/kotlin/com/mrl/pixiv/main.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import coil3.network.ktor3.KtorNetworkFetcherFactory
 import com.mrl.pixiv.common.network.ImageClient
+import com.mrl.pixiv.common.repository.SettingRepository
 import com.mrl.pixiv.common.util.RStrings
 import com.mrl.pixiv.di.Initialization
 import com.mrl.pixiv.strings.app_name
@@ -12,10 +13,12 @@ import io.ktor.client.HttpClient
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
 import org.koin.core.qualifier.named
+import java.util.Locale
 
 fun main() {
     FileKit.init(appId = "PiPixiv")
     Initialization.initKoin()
+    setDefaultLocale()
     application(exitProcessOnExit = false) {
         val appName = stringResource(RStrings.app_name)
 
@@ -33,4 +36,10 @@ fun main() {
             )
         }
     }
+}
+
+private fun setDefaultLocale() {
+    val (language, region) = SettingRepository.userPreferenceFlow.value.appLanguage?.split("-")
+        ?.let { it[0] to it.getOrNull(1).orEmpty() } ?: return
+    Locale.setDefault(Locale.of(language, region))
 }
