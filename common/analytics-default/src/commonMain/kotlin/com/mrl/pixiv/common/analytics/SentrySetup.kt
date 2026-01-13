a7dd0878576b54cc6a7bd1cb15f6eac858414575
Darrious <60057825+master-lzh@users.noreply.github.com>
2026-01-13 20:01:17 +0800

    feat(analytics): 修正Sentry初始化默认逻辑
    - 修改`initializeSentry`方法中关于`isDebug`判断的逻辑，确保仅在非调试模式下初始化Sentry。
    - 优化错误监控功能的初始化条件，避免调试模式中额外的请求开销或误报。

--- common/analytics-default/src/commonMain/kotlin/com/mrl/pixiv/common/analytics/SentrySetup.kt
+++ common/analytics-default/src/commonMain/kotlin/com/mrl/pixiv/common/analytics/SentrySetup.kt
@@ -3,7 +3,7 @@ package com.mrl.pixiv.common.analytics
 import io.sentry.kotlin.multiplatform.Sentry
 
 fun initializeSentry(isDebug: Boolean, dsn: String) {
-    if (isDebug) {
+    if (!isDebug) {
         Sentry.init { options ->
             options.dsn = dsn
             options.sendDefaultPii = false
