6366b082e9b41c760243d8e3410ce78fb4c804d1
Darrious <60057825+master-lzh@users.noreply.github.com>
2026-03-06 15:12:03 +0800

    fix(initialization): 增强MMKV初始化的错误处理机制
    - 为`initializeMMKV`方法增加try-catch块以捕获异常，提高稳定性。
    - 在异常情况下调用`exitProcess`确保应用程序安全退出，防止未处理异常导致的意外行为。

--- composeApp/src/jvmMain/kotlin/com/mrl/pixiv/di/Initialization.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/mrl/pixiv/di/Initialization.jvm.kt
@@ -2,11 +2,18 @@ package com.mrl.pixiv.di
 
 import com.ctrip.flight.mmkv.MMKVLogLevel
 import com.ctrip.flight.mmkv.initialize
+import com.mrl.pixiv.common.analytics.logException
 import io.github.vinceglb.filekit.FileKit
 import io.github.vinceglb.filekit.absolutePath
 import io.github.vinceglb.filekit.div
 import io.github.vinceglb.filekit.filesDir
+import kotlin.system.exitProcess
 
 actual fun initializeMMKV(logLevel: MMKVLogLevel) {
-    initialize((FileKit.filesDir / "mmkv").absolutePath(), logLevel)
+    try {
+        initialize((FileKit.filesDir / "mmkv").absolutePath(), logLevel)
+    } catch (e: Exception) {
+        logException(e)
+        exitProcess(-1)
+    }
 }
