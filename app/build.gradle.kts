1b4cb72618dc2963734fddbda2b8bfbcbd6cc2b0
Darrious <60057825+master-lzh@users.noreply.github.com>
2026-03-19 17:33:20 +0800

    feat(dependencies): 更新第三方库版本以提高兼容性和性能
    - 升级kotlin、datastore、paging等版本以获得最新功能和修复
    - 更新Podfile中MMKV的版本以提高稳定性
    - 调整Gradle插件逻辑，优化sentry插件应用条件
    - 更新多个库版本，包括okio、koin等，提高整体兼容性和应用性能

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,6 @@ import org.gradle.internal.extensions.stdlib.capitalized
 plugins {
     id("pixiv.android.application")
 //    alias(androidx.plugins.baselineprofile)
-    alias(libs.plugins.sentry.android) apply false
 }
 if (project.findProperty("applyFirebasePlugins") == "true") {
     pluginManager.apply(libs.plugins.sentry.android.get().pluginId)
@@ -144,7 +143,7 @@ dependencies {
     implementation(libs.mmkv.kotlin)
 }
 
-pluginManager.withPlugin(libs.plugins.sentry.android.get().pluginId) {
+if (pluginManager.hasPlugin(libs.plugins.sentry.android.get().pluginId)) {
     configure<SentryPluginExtension> {
         debug.set(true)
         org.set("pipixiv")
