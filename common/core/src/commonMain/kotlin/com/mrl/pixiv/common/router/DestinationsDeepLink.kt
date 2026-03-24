22c185c3753a6158e05c1723289a69b059a3a3e1
Darrious <60057825+master-lzh@users.noreply.github.com>
2026-03-24 14:17:21 +0800

    feat(deeplink): 支持小说详情页的深度链接导航
    - 在`AndroidManifest.xml`中增加`/novel/`的路径前缀以支持小说详情页的深度链接
    - 更新`Navigation3MainGraph.android.kt`以检测和处理小说详情的深度链接导航
    - 在`DestinationsDeepLink.kt`中新增小说链接的正则表达式以匹配小说详情页的URL格式

--- common/core/src/commonMain/kotlin/com/mrl/pixiv/common/router/DestinationsDeepLink.kt
+++ common/core/src/commonMain/kotlin/com/mrl/pixiv/common/router/DestinationsDeepLink.kt
@@ -3,4 +3,6 @@ package com.mrl.pixiv.common.router
 object DestinationsDeepLink {
     val illustRegex = "http(s)?://(www\\.)?pixiv\\.(net|me)(/.*)?/artworks/(\\d+)".toRegex()
     val userRegex = "http(s)?://(www\\.)?pixiv\\.(net|me)(/.*)?/users/(\\d+)".toRegex()
+
+    val novelRegex = "http(s)?://(www\\.)?pixiv\\.(net|me)(/.*)?/novel/show\\.php\\?id=(\\d+)".toRegex()
 }
